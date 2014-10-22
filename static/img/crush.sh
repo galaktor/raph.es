a2579f827dbe544e954516e646fa55037670587a
Raphael Estrada <galaktor@gmx.de>
2014-10-22 17:51:02 +0100

    good watermark and thumbs

--- static/img/crush.sh
+++ static/img/crush.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 for img in **/*.* **/**/*.*; do
   # todo: exclude files already converted; e.g. by adding "wm" to file name or similar
   echo processing \"$img\"
@@ -13,10 +15,26 @@ for img in **/*.* **/**/*.*; do
       echo "already watermarked; skipping"
       ;;
     *)
-      echo "todo: convert"
+      echo "watermarking $img"
       ext="_wm.jpeg"
       target="$base$ext"
-      convert "$img" -quality 95 "$target"
+      width=$(identify "$img" | cut -d" " -f3 | cut -dx -f1)
+      height=$(identify "$img" | cut -d" " -f3 | cut -dx -f2)
+      size=$(($height/45))
+      # first pass, black text
+      convert "$img" -pointsize "$size" -gravity southeast -fill "#000000" -draw "text 5,5 '(c) 2014 http://raph.es'" -quality 100 "$target"
+      # second pass, white text and some compression
+      convert "$target" -pointsize "$size" -gravity southeast -fill "#FFFFFF" -draw "text 6,6 '(c) 2014 http://raph.es'" -quality 95 "$target"
+      # thumbnail
+      echo "thumbnailing $img"
+      ext="_thumb.jpeg"
+      target="$base$ext"
+      if [ $width -gt $height ]
+	  then
+  	    convert "$img" -resize 300x -quality 95 "$target"
+          else
+            convert "$img" -resize x300 -quality 95 "$target"
+      fi
       ;;
   esac
 done
