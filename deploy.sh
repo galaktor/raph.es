c8ac7ac2873f47460546fb7f1ba06764cb6fe7c8
Raphael Estrada <galaktor@gmx.de>
2014-11-05 16:12:32 +0000

    use repo clone instead of subtrees

--- deploy.sh
+++ deploy.sh
@@ -6,38 +6,39 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # configure git credentials
-echo "setting git credentials $GIT_NAME $GH_TOKEN"
+echo "*** setting git credentials $GIT_NAME $GH_TOKEN"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git config user.name $GIT_NAME
 
-# refresh and remove subtree
-git remote add out https://github.com/galaktor/galaktor.github.io
-git subtree pull --prefix=output out master
-
 # clear output
-git rm output -rf
+echo "*** clearing output"
+rm output -rf
+
+# refresh and remove subtree
+echo "*** cloning output repo"
+git clone https://github.com/galaktor/galaktor.github.io output
+cd output
+rm * -rf
+cd ..
 
 # Build the project. 
-echo "building html"
+echo "*** building html"
 ./hugo
 
 # Add changes to git.
-echo "adding changes to git"
-git add output
-
-# restore files we don't want to change
-# todo: move insto 'static'
-git checkout output/CNAME output/LICENSE output/README.md
+echo "*** adding changes to git"
+cd output
+git add -A
 
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-echo "commiting: $msg"
+echo "*** commiting: $msg"
 git commit -m "$msg"
 
 # push subtree
-echo "pushing subtree to gh pages"
-git subtree push --prefix=output out master
+echo "*** pushing to gh pages"
+git push
