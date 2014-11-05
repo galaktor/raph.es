bda1c9c23d0a5ab87305cd158cc7b0f399decb08
Raphael Estrada <galaktor@gmx.de>
2014-11-05 10:43:58 +0000

    set creds before pull

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# configure git credentials
+echo "setting git credentials $GIT_NAME $GH_TOKEN"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+git config user.name $GIT_NAME
+
 # refresh and remove subtree
 git remote add out https://github.com/galaktor/galaktor.github.io
 git subtree pull --prefix=output out master
@@ -15,12 +21,6 @@ rm -rf output
 # Build the project. 
 echo "building html"
 ./hugo
-
-# configure git credentials
-echo "setting git credentials $GIT_NAME $GH_TOKEN"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-git config user.name $GIT_NAME
 
 # Add changes to git.
 echo "adding changes to git"
