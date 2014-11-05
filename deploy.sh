54a4d53c556c7194a0fdc27db3979f696bd3174a
Raphael Estrada <galaktor@gmx.de>
2014-11-05 10:20:11 +0000

    add some prints for debugging

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,17 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 rm -r output
 
 # Build the project. 
+echo "building html"
 ./hugo
 
 # configure git credentials
+echo "setting git credentials $GH_NAME $GH_TOKEN"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git config user.name $GH_NAME
 
 # Add changes to git.
+echo "adding changes to git"
 git add .
 
 # Commit changes.
@@ -24,7 +27,9 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+echo "commiting: $msg"
 git commit -m "$msg"
 
 # push subtree
+echo "pushing subtree to gh pages"
 git subtree push --prefix=output https://github.com/galaktor/galaktor.github.io master
